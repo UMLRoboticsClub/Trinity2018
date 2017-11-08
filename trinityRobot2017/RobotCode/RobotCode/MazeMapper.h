491aee359ded730bc12aecfad95cf9acd37e4985
U-MATTLAPTOP\matt <mpelland@mitre.org>
2017-11-07 20:10:21 -0500

    MazeMapper header

--- trinityRobot2017/RobotCode/RobotCode/MazeMapper.h
+++ trinityRobot2017/RobotCode/RobotCode/MazeMapper.h
@@ -1,8 +1,22 @@
 #pragma once
+#include<vector>
+#include "GameState.h"
+using namespace std;
 class MazeMapper
 {
 public:
 	MazeMapper();
+private:
 	~MazeMapper();
+	//vector<Point> is sequence of waypoints
+	vector<Point> findNextTarget(GameState state); //only function called by the robot
+	void createTargetPath();//updates distanceField
+
+	void updateOccupancyGrid(); //gets laser data and updates grid potentiall have running on interrupt somehow whenever we get a laser scan
+	void computeDistanceField(GameState state); //takes gamestate or type of target, called in find 
+
+	OccupancyGrid occGrid;
+	vector<vector<int>> distanceField;
+	LaserScanner scanner;
 };
 
