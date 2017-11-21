30d180db19f0edd5375e226a48250c46cfb3b23c
Sam <samuelkovaly05@gmail.com>
2017-11-20 19:03:12 -0500

    made thread in Robot.cpp, changed MazeMapper so that child threading will work as intended

--- trinityRobot2017/RobotCode/RobotCode/Robot.cpp
+++ trinityRobot2017/RobotCode/RobotCode/Robot.cpp
@@ -1,11 +1,29 @@
 #include "Robot.h"
+#include <thread>
 
-Robot::Robot() {}
+using namespace std;
+
+
+// made using this video: https://www.youtube.com/watch?v=LL8wkskDlbs
+
+Robot::Robot():
+	robotPos(0), robotAngle(0), mazeMapper(), drive(), gameState(),
+	safeZoneLocation(), colorSensor(), IRsensor(), camera()
+{
+	// variables are initialized through the constructor for now
+}
 
 ///////////////////////////
 
-void Robot::start(void) {}
+void Robot::start(void) {
+	// maybe this can go in the constructor in the future
+	// Thread dedicated to looping the lazer scanner until the robot dies.
+	laserScanInputThread(mazeMapper.laserScanLoop);
+	laserScanInputThread.detach(); // thread should run freely on its own ( this function doesn't wait for it to finish)
+}
 
 ///////////////////////////
 
-void Robot::robotLoop(void){}
+void Robot::robotLoop(void){
+
+}
