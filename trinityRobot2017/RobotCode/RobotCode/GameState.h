68b3b0bc0e8e1911d6f3049cc42a144bb3a64eaa
U-MATTLAPTOP\matt <mpelland@mitre.org>
2017-12-05 17:29:47 -0500

    tried to get work done and made more work instead. DistanceField function complete.

--- trinityRobot2017/RobotCode/RobotCode/GameState.h
+++ trinityRobot2017/RobotCode/RobotCode/GameState.h
@@ -1,5 +1,6 @@
 #pragma once
-
+#include<vector>
+#include "constants.h"
 
 struct GameState {
 	int levelOfCompetition;
@@ -7,4 +8,32 @@ struct GameState {
 	bool babyObtained;
 	bool safeZoneFound;
 	bool inRoom;
+	bool secondArena;
+
+	vector<int> getTargetType() {
+		//returns what kind of objects (in order or priority) to look for in the important object map before resorting to exploring
+		vector<int> targets;
+		if (levelOfCompetition != 3 || babySaved) {
+			targets.push_back(FLAME);
+			targets.push_back(CANDLE);
+			targets.push_back(DOOR);
+			return targets;
+		}
+			
+		//level 3, baby not saved
+		if (babyObtained) {
+			//we have the baby we just need to shove it out the window
+			targets.push_back(SAFE_ZONE);
+			return targets;
+		}
+		//we have not found the baby
+		else {
+			if (!secondArena)
+				targets.push_back(HALLWAY);
+			else {
+				targets.push_back(DOOR);
+			}
+		}
+		return targets;
+	}
 };
--- trinityRobot2017/RobotCode/RobotCode/Point.h
+++ trinityRobot2017/RobotCode/RobotCode/Point.h
@@ -1,4 +1,9 @@
+#pragma once
+
 struct Point {
+	Point() {}
+	Point(int _x, int _y) { x = _x; y = _y; }
+	Point(DoublePoint p) { x = static_cast<int>(p.x); y = static_cast<int>(p.y); }
 	int x;
 	int y;
 };
