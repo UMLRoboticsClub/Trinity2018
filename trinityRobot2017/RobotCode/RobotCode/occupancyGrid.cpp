afb7e7e20d70ae15f0c2ff8135e24931bbb04604
Sam <samuelkovaly05@gmail.com>
2017-12-04 19:56:41 -0500

    Implemented occupancyGrid.cpp and found myself along the way

--- trinityRobot2017/RobotCode/RobotCode/occupancyGrid.cpp
+++ trinityRobot2017/RobotCode/RobotCode/occupancyGrid.cpp
@@ -1,31 +1,35 @@
 /* 
  * File:   occupancyGrid.cpp
- * Author: Mong Mary Touch
+ * Author: Mong Mary Touch     & SAM
  * 
  * Created on November 7, 2017
  */
 
-// store realX and realY into an array
 #include "occupancyGrid.h"
 #include "Point.h"
+//#include kimJong-Un.h
 
-OccupancyGrid::OccupancyGrid() {
-    
+using namespace std;
+
+OccupancyGrid::OccupancyGrid(): width(500), height(500), resolution(1) {
+
+	// initalize them NavVals
+	gridVals( int(width * resolution), vector<NavVal>(int(height * resolution), NavVal()) );
+
 }
 
 OccupancyGrid::OccupancyGrid(const OccupancyGrid& orig) {
-
+	// too lazy to make this
 }
 
 int OccupancyGrid::update(float realX, float realY, int val) {
-    
+	gridVals[int(realX * resolution)][int(realY * resolution)].updateValue(val);
 }
 
 int OccupancyGrid::getValue(int x, int y) {
-    
-    
+	return gridVals[x][y];
 }
 
-int OccupancyGrid::getValue(Point) {
-    
+int OccupancyGrid::getValue(Point point) {
+	return gridVals[point.x, point.y];
 }
