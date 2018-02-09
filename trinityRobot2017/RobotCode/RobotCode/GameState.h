11f99e277d8ae525a5561a7682ae5f44bc206bd8
U-MATTLAPTOP\matt <mpelland@mitre.org>
2018-02-08 19:44:53 -0500

    WIP

--- trinityRobot2017/RobotCode/RobotCode/GameState.h
+++ trinityRobot2017/RobotCode/RobotCode/GameState.h
@@ -16,6 +16,18 @@ struct GameState {
 		secondArena = _secondArena;
 	}
 
+	//in level 3, there are a total of 3 candles.  1 in B, lit at the start,
+	//and 2 in A - one lit after 90 seconds, one lit after 120 seconds
+	//as far as I can tell there's only one candle in levels 1 and 2
+
+	//when completely done with arena B might be worth it to "wall it off" so we don't accidentally go back there cuz we're stupid.
+	//though that shouldn't be an issue.  I hope.  Maybe it would be.
+
+	//avoid going into rooms when on level 3 and in arena 1.  Do we need a thing
+	//to specify NOT to go into room?  How to do, how to do...
+
+	//we need to know more info....  this is a pain in the ass
+
 	int levelOfCompetition;
 	int numCandlesExtinguished;
 	bool babySaved;
@@ -27,6 +39,24 @@ struct GameState {
 	vector<int> getTargetType() {
 		//returns what kind of objects (in order or priority) to look for in the important object map before resorting to exploring
 		vector<int> targets;
+		//if we are level 1 or 2 and have extinguished the candle
+		if(levelOfCompetition != 3 && numCandlesExtinguished > 0){
+			targets.push_back(START_ZONE);
+			return targets;
+		}
+
+		//level 3 and all candles extinguished
+		if(levelOfCompetition == 3 && numCandlesExtinguished == 3){
+			targets.push_back(START_ZONE);
+			return targets;
+		}
+
+		//we have extinguished the one candle in arena B, return to A
+		if(levelOfCompetition == 3 && numCandlesExtinguished == 1 && secondArena){
+			targets.push_back(HALLWAY);
+			return targets;
+		}
+
 		if (levelOfCompetition != 3 || babySaved) {
 			targets.push_back(FLAME);
 			targets.push_back(CANDLE);
@@ -48,7 +78,7 @@ struct GameState {
 				targets.push_back(RED_SIDE_CRADLE);
 				targets.push_back(BLUE_SIDE_CRADLE);
 				targets.push_back(GREEN_SIDE_CRADLE);
-				targets.push_back(DOOR);//no need to differentiate entering and exiting room here, that's done outside
+				targets.push_back(DOOR);//no need to differentiate entering and exiting room here, that's done in MazeMapper
 			}
 		}
 		return targets;
