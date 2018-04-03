d7e9082ec60614f22bcc41f6e81172aa99b777d7
Jackson Seal <sealj553@gmail.com>
2018-04-03 00:16:24 +0000

    didn't expect this to work, but it do.  Much good

---
+++ trinityRobot2017/RobotCodeMake/opticalflow.cpp
@@ -0,0 +1,76 @@
+#include "opticalflow.h"
+#include<iostream>
+using std::cout;
+using std::endl;
+
+void opticalFlow::init(){
+    handle = spi_open(0, 0, 2e6, 0);
+    if(handle < 0){
+        cout << "that'll do it: " << handle << endl;
+        cout << (handle == PI_BAD_SPI_CHANNEL) << endl;
+        cout << (handle == PI_BAD_SPI_SPEED) << endl;
+        cout << (handle == PI_BAD_FLAGS) << endl;
+        cout << (handle == PI_NO_AUX_SPI) << endl;
+        cout << (handle == PI_SPI_OPEN_FAILED) << endl;
+        exit(1);
+    }
+    gpio_write(0, PIN_MOUSECAM_CS, 1);
+    reset();
+    writeReg(ADNS3080_CONFIGURATION_BITS, 0x19);
+}
+void opticalFlow::reset(){
+    gpio_write(0, PIN_MOUSECAM_RESET, 1);
+    time_sleep(.001);
+    gpio_write(0, PIN_MOUSECAM_RESET, 0);
+    time_sleep(.035);
+}
+DoublePoint opticalFlow::readMotion(){
+    MD md;
+    gpio_write(0, PIN_MOUSECAM_CS, 0);
+    transferBits(ADNS3080_MOTION_BURST);
+    time_sleep(0.000075);
+    md.motion = transferBits(0xff);
+    md.dx = transferBits(0xff);
+    md.dy = transferBits(0xff);
+    md.squal = transferBits(0xff);
+    md.shutter = transferBits(0xff) << 8;
+    md.shutter |= transferBits(0xff);
+    md.max_pix = transferBits(0xff);
+    gpio_write(0, PIN_MOUSECAM_CS, 1);
+    
+    DoublePoint data(md.dx, md.dy);
+    if(data.x > 127)
+        data.x = 256-data.x;
+    if(data.y > 127)
+        data.y = 256-data.y;
+    return data;
+}
+void opticalFlow::writeReg(int reg, int val){
+    gpio_write(0, PIN_MOUSECAM_CS, 0);
+    transferBits(reg | 0x80);
+    transferBits(val);
+    gpio_write(0, PIN_MOUSECAM_CS, 1);
+    time_sleep(.00005);
+}
+int opticalFlow::readReg(int reg){
+    gpio_write(0, PIN_MOUSECAM_CS, 0);
+    transferBits(reg);
+    time_sleep(.000075);
+    int ret = transferBits(0xff);
+    gpio_write(1, PIN_MOUSECAM_CS, 1);
+    time_sleep(000001);
+    return ret;
+}
+
+char opticalFlow::transferBits(char toTransfer){
+    char ret = 7;
+    spi_xfer(0, handle, &toTransfer, &ret, 1);
+    return ret;
+}
+
+char opticalFlow::transferBits(int toTransfer){
+    char ret = 7;
+    char toTransferC = char(toTransfer);
+    spi_xfer(0, handle, &toTransferC, &ret, 1);
+    return ret;
+}
